4bcb8d20c75eaa77e4fe0d0e82830e43f2507ffb
farjunShiran <devshiranf@gmail.com>
2022-12-12 12:12:00 +0200

    firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyBqczGq_yv0UxL9nkef3VtEA9kRkrbggag",
+    authDomain: "chatapp-93718.firebaseapp.com",
+    projectId: "chatapp-93718",
+    storageBucket: "chatapp-93718.appspot.com",
+    messagingSenderId: "890362262727",
+    appId: "1:890362262727:web:de2a4b974e5e47dff1fae2",
+    measurementId: "G-FVX7K825ME"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyBqczGq_yv0UxL9nkef3VtEA9kRkrbggag",
+    authDomain: "chatapp-93718.firebaseapp.com",
+    projectId: "chatapp-93718",
+    storageBucket: "chatapp-93718.appspot.com",
+    messagingSenderId: "890362262727",
+    appId: "1:890362262727:web:de2a4b974e5e47dff1fae2",
+    measurementId: "G-FVX7K825ME"
+  }
 };
 
 /*
